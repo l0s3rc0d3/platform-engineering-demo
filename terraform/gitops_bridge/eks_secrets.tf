21626cdcb7d2bb7a59ff3b75c875bcc21d12a4da
l0s3rc0d3 <giorgio.scampini.h@gmail.com>
2026-03-12 14:28:59 +0100

    feat(gitops): better comments to explain how the secret works

--- terraform/gitops_bridge/eks_secrets.tf
+++ terraform/gitops_bridge/eks_secrets.tf
@@ -8,8 +8,8 @@ resource "kubernetes_secret_v1" "argocd_cluster_metadata" {
 
     # Labels used as selectors in ApplicationSet to enable/disable addon for specific clusters
     labels = {
-      "argocd.argoproj.io/secret-type"        = "cluster"
-      "enable_external_secrets"               = "true"
+      "argocd.argoproj.io/secret-type"        = "cluster" # this label enable argo to load this secret
+      "enable_external_secrets"               = "true" # this enable the gitops modularity feature and you can choose which addon to enable 
       "enable_aws_load_balancer_controller"   = "true"
     }
 
@@ -25,9 +25,10 @@ resource "kubernetes_secret_v1" "argocd_cluster_metadata" {
     )
   }
 
+  # the following tell argo how to connect to the cluster
   data = {
     name   = "in-cluster"
-    server = "https://kubernetes.default.svc"
+    server = "https://kubernetes.default.svc" # this parameter is used inside each application set!
     config = jsonencode({ tlsClientConfig = { insecure = false } })
   }
 
